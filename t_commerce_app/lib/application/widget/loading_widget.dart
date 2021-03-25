5d163cb92071c2c29207fa9eab29d9cb7e483a61
Bui Quang Tri <tri.bui@monstar-lab.com>
2021-03-25 21:19:28 +0700

    [tasks.1.0] add loading screen

--- t_commerce_app/lib/application/widget/loading_widget.dart
+++ t_commerce_app/lib/application/widget/loading_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatefulWidget {
   @override
@@ -9,7 +10,35 @@ class _LoadingWidgetState extends State<LoadingWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("welcome"),
+      backgroundColor: Colors.blue[900],
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 100),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: SpinKitCubeGrid(
+                color: Colors.white,
+                size: 100,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome to T Commerce!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
