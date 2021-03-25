3ff41225ca42e9db32da5ef3ddd218fe720c2e4e
Bui Quang Tri <tri.bui@monstar-lab.com>
2021-03-25 20:05:20 +0700

    add folder structure

---
+++ t_commerce_app/lib/application/app/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:t_commerce_app/application/app/my_app.dart';
+
+void main() => runApp(MyApp());
---
+++ t_commerce_app/lib/application/app/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:t_commerce_app/application/widget/loading_widget.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'T Commerce',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadingWidget(),
+    );
+  }
+}
